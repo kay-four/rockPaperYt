f7f84a46765fe2e081e09fccf723f9c5bd2515f8
kay-four <88488188+kay-four@users.noreply.github.com>
2022-06-03 17:11:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import random
+
+
+
+choices = ["R","P","S"]
+
+computer = random.choice(choices)
+player = input("Select 'R'(rock), 'P'(paper) or 'S'(scissors):\n").upper()
+while player not in choices:
+   print("Error! Invalid entry!")
+   player = input("Select 'R'(rock), 'P'(paper) or 'S'(scissors):\n").upper()
+else:
+   while player == computer:
+      computer = random.choice(choices)
+      player = input("Select 'R'(rock), 'P'(paper) or 'S'(scissors):\n").upper()
+   else:
+      print("Player(",player,") : CPU(",computer,")")
+    
+if player != computer:
+ if player == "R":
+   if computer== "P":
+      print("Computer wins! It picked: ",computer)
+   elif computer == "S":
+      print("You win! Computer picked: ",computer) 
+ elif player == "P":
+   if computer== "S":
+      print("Computer wins! It picked: ",computer)
+   elif computer== "R":
+      print("You win! Computer picked: ",computer)
+ elif player == "S":
+   if computer== "R":
+      print("Computer wins! It picked: ",computer) 
+   elif computer == "P":
+      print("You win! Computer picked",computer)
+
+   
+
+
+# def is_it_a_tie(computer,player):
+#  if player == computer:
+   #   print("Tie!/n Play again")
+      #  take_input()
+
+
+
+
+
+
+ 
+        
+# print("The computer picked: ",computer)
+# print("Player selected: ",player)
+ 
+ 
